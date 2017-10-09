1bc42b399551e572949f38991da1ce3400f749d1
Jim Leach <jimmy22theave@gmail.com>
2017-10-09 15:57:16 +0100

    Implement addin to create a work product

--- R/create_wp.R
+++ R/create_wp.R
@@ -1,114 +1,90 @@
 #' Create a new work product.
 #'
-#' This function creates the skeleton for a new work-product within the project.
-#' It must be called from the root directory of the project.
-#' @param title The title of the work product.
-#' @param owner The name of the owner of the work product (i.e. the analyst
-#'   reponsble).
-#' @param deliver_to The name of the person the work product will be delivered
-#'   to/is for.
-#' @param remark A brief sentence describing the work product.
-#' @param wp_dir The name of the work product directory. Defaults to wp.
-#' @export
 #' @references "Guerrilla Analytics: A Practical Approach to Working with Data", Enda Ridge (\href{https://guerrilla-analytics.net/}{Website})
-#' @examples
-#' create_wp("wp", "Customer complaints summary", "Jim Leach", "Enda Ridge", "Summary numbers for customer complaints from the past 6 months, after removing products A, B ande C.")
 
-create_wp <- function(title, owner, deliver_to, remark, wp_dir = "wp") {
-
-  # If wp directory doesn't exist, throw an error
-  if (!dir.exists(wp_dir)) {
-    stop("The provided work product directory does not exist.")
-  }
-
-  # First, list all the files in the wp directory
-  wp_all <- list.files(file.path(".", wp_dir))
-  wp_all <- wp_all[-grep("wp_log.csv", wp_all)]
-
-  # Count how many there are
-  how_many_exist <- length(wp_all)
-
-  # Increment that by 1 to create the new workproduct
-  new <- how_many_exist + 1
-
-  # Set up the wp filename
-  wp_path <- file.path(".", wp_dir, sprintf("wp%04d", new))
-
-  # Create the directory as long as it doesn't already exist (if it does throw an error)
-  dir.create(wp_path)
-
-  # Set up details to write a brief readme
-  h1 <- paste("# Work Product", new, "-", title)
-  dt <- paste("Created on:", Sys.Date())
-  own <- paste("Created by", owner)
-  del <- paste("For delivery to:", deliver_to)
-  com <- paste("## Details\n", remark)
-
-  # Create md file and path
-  md_file <- c(h1, dt, "", own, "", del, "", com)
-  md_file_path <- file.path(wp_path, "readme.md")
-
-  # Create a simple readme file
-  writeLines(md_file, con = md_file_path)
-
-  # Write to the log file
-  # Create the log if it doesn't exist
-  if (how_many_exist == 0) {
-    df <- data.frame(wp = character(),
-                     created = character(),
-                     created_by = character(),
-                     created_for = character(),
-                     note = character(),
-                     stringsAsFactors=FALSE)
-
-    # set path to log
-    log_path <- file.path(".", wp_dir, "wp_log.csv")
-    write.table(df, file = log_path, row.names = FALSE,
-                sep = ",")
-  }
-
-  # Create the wp log entry
-  log_entry <- cbind(sprintf("wp%04d", new),
-                 as.character(Sys.Date()),
-                 owner,
-                 deliver_to,
-                 remark)
-
-  # set log file path
-  log_file <- file.path(".", wp_dir, "wp_log.csv")
-
-  # Write the entry
-  write.table(log_entry,
-              file = log_file,
-              append = TRUE,
-              row.names = FALSE,
-              col.names = FALSE,
-              sep = ",")
-
-}
 
 create_wp <- function() {
-  # Path to wp tracker
-  wp_path <- file.path(".", "wp", "work-products-tracker.csv")
 
-  # Read the work product tracker
-  current_tracker <- read.csv(wp_path, stringsAsFactors = FALSE)
-
-  current_wp <- data.frame(
-    name = "Test",
-    prepared_by = "Jim",
-    delivered_to = "Jo",
-    version = max(1, current_tracker$version + 1),
-    last_updated = as.character(Sys.Date()),
-    comments = "A test comment",
-    stringsAsFactors = FALSE
+  # Create the UI --------------------------------------------------------------
+  wp_ui <- miniUI::miniPage(
+    miniUI::miniContentPanel(
+      ## Add UI items
+      shiny::textInput("name", "Work product name"),
+      shiny::textInput("prep", "Prepared by"),
+      shiny::textInput("del", "For delivery to"),
+      shiny::textInput("comment", "Additional comments"),
+      miniUI::gadgetTitleBar(title = NULL)
+    )
   )
 
-  # Combine them
-  new_tracker <- rbind(current_tracker, current_wp)
+  # Create the server functionality --------------------------------------------
+  wp_server <- function(input, output, session) {
 
-  # Write back out to file
-  write.csv(new_tracker, wp_path, row.names = FALSE)
+    # When the function runs, set up the right path, and read in the current tracker
+    wp_path <- file.path(".", "wp")
+
+    # Check if the tracker exists - if it doesn't we can't do much!
+    if ( !file.exists(wp_path) ) {
+      message("Work products folder does not exist. Did you initialise the project with one?")
+      shiny::stopApp()
+    }
+
+    # Listen for the 'done' event. This event will be fired when a user
+    # is finished interacting with your application, and clicks the 'done'
+    # button.
+    shiny::observeEvent(input$done, {
+
+      tracker_path <- file.path(wp_path, "work-products-tracker.csv")
+      # Get the current tracker
+      current_tracker <- read.csv(
+        tracker_path,
+        stringsAsFactors = FALSE
+        )
+
+      # Set up details of the current work product using the inputs, and some defaults
+      current_wp <- data.frame(
+         id = max(1, current_tracker$id + 1),
+         name = input$name,
+         prepared_by = input$prep,
+         delivered_to = input$del,
+         version = 1,
+         last_updated = as.character(Sys.Date()),
+         comments = input$comment,
+         stringsAsFactors = FALSE
+       )
+
+      # Combine the old tracker with the new work product
+      new_tracker <- rbind(current_tracker, current_wp)
+
+      # Sort by ID
+      ord <- order(new_tracker$id)
+      new_tracker <- new_tracker[ord, ]
+
+      # Write back out to file
+      write.csv(new_tracker, tracker_path, row.names = FALSE)
+
+      # Get the numeric ID of the newly-created wp
+      id <- current_wp$id
+
+      # Turn it into a wp-id in 3-number form
+      id <- sprintf("%03d", id)
+
+      # Make the directory to put it in
+      dir.create(file.path(wp_path, id))
+
+      # Close the app
+      shiny::stopApp()
+    })
+
+    shiny::observeEvent(input$cancel, {
+      shiny::stopApp()
+    })
+  }
+
+
+  # Run the app ----------------------------------------------------------------
+  viewer <- shiny::dialogViewer("Create a new work product")
+  shiny::runGadget(wp_ui, wp_server, viewer = viewer, stopOnCancel = FALSE)
 }
 
 
